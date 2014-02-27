557cb17ec11148ec5ace5b0b65b2a5cb1f05b782
MELISSA HSU <MELISSAHSU@BERKELEY.EDU>
2014-02-26 17:15:47 -0800

    Completed Disappear problem!

--- static/js/stuff.js
+++ static/js/stuff.js
@@ -8,7 +8,7 @@ $('#say_hello').click(function() {alert('Hello World!')
 
 // Problem 2 (Houdini) ------------------------------------------------------
 $('#disappear').click(function() {
-  //WRITE CODE HERE
+	$('other').hide('Write some code to make this paragraph disappear when you click on the button. Then write some code to make it reappear with the other button');
 });
 
 $('#reappear').click(function() {
