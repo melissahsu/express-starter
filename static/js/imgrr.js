07a0f5ba4a11d13ab4736096a84c815b3be0cebd
MELISSA HSU <MELISSAHSU@BERKELEY.EDU>
2014-03-05 18:56:54 -0800

    Mastered right angles!

--- static/js/imgrr.js
+++ static/js/imgrr.js
@@ -1,13 +1,25 @@
 // Problem 2 (Peekaboo) ------------------------------------------------------
 // WRITE CODE HERE
 
+$('#toggle_img').click(function() {
+	if ( $'#toggle_img').text() == 'Come Back!' {
+		$('#toggle_img').text ('Go Away!');
+		$('#main_img').show();
+	}
+});
 // Problem 3 (Swap Em) -----------------------------------------------
 // WRITE CODE HERE
+$('#change_img').click(function() {
+	$('#main_img').attr('src','/static/img/' +
+		$('new_img_file').val())
+});
 
 // Problem 4 (Find the Source) -------------------------------------------------
 $('.clickable').click(function() {
-  // WRITE CODE HERE
+   alert($(this).attr('src'));
 });
 
 // Problem 5 (Imgrr) -------------------------------------------------
-// WRITE CODE HERE
+$('#clack').click(function() {
+	$('#bigimage').attr('src',$(this).attr('src'));
+});
